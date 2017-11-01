5abcf438f004423f411ec52e80bcb21cafd4b93c
Tobias Wilson <tw315@kent.ac.uk>
2017-11-01 18:58:24 +0000

    testing and adding first script

---
+++ newUsers.ps1
@@ -0,0 +1,25 @@
+﻿import-module activedirectory
+$givenname = Read-Host -Prompt 'What is their first name?'
+$surname = Read-Host -Prompt 'What is their second name?'
+$confirmed = 1
+while($confirmed)
+{
+$password = Read-Host  -Prompt 'Enter Password' -AsSecureString
+$password2= Read-Host  -Prompt 'Enter Password Again' -AsSecureString
+$pwd1_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+$pwd2_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password2))
+if($pwd1_text -eq $pwd2_text){
+$confirmed = 0
+}
+else{$a = new-object -comobject wscript.shell
+
+$b = $a.popup(“Try again, you muppet “,0,“Passwords did not match”,1)}
+}
+
+$username = $givenname + $surname.substring(0,1)
+$displayName = $givenname + " " + $surname
+
+
+
+-path = "OU=x (e.g london watchguard")
+
